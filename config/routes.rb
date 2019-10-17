6d5d80a34e6fc29c3d726edec9d416b96446c765
Liam <liam.healy1@marist.edu>
2019-10-17 13:44:35 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  resources :dogs
+  resources :dogs do
+    collection do
+      get :most_employees
+    end
+  end
   resources :employees
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
