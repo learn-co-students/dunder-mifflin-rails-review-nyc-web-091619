7ae3696e5919db7c2d31538f9259ca9840613749
github username <github email address>
2019-10-17 13:24:15 -0400

    Done.

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,56 @@
 class DogsController < ApplicationController
+
+    def index
+        @dogs = Dog.all
+    end
+
+    def new
+        @dog = Dog.new
+        @employees = Employee.all 
+    end
+
+    def show
+        @dog = Dog.find(params[:id])
+    end
+
+    def create
+        @dog = Dog.new(dog_params)
+        @employees = Employee.all
+        if @dog.save
+            redirect_to dog_path(@dog)
+        else
+            # redirect_to new_dog_path
+            render :new
+        end
+    end
+
+    def edit
+        @dog = Dog.find(params[:id])
+    end
+
+    def update
+        @dog = Dog.find(params[:id])
+        @dog.update(dog_params)
+
+        if @dog.save
+            redirect_to dog_path(@dog)
+        else
+            # redirect_to new_dog_path
+            render :new
+        end
+    end
+
+    def destroy
+        @dog = Dog.find(params[:id])
+        @dog.destroy
+
+        # redirect_to new_dog_path
+        render :new
+    end
+
+    private
+
+    def dog_params
+        params.require(:dog).permit(:name, :breed, :age)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   resources :dogs
   resources :employees
+
+
+  # get '/blah', 'somecontroller#method'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
