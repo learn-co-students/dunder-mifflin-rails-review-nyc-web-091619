b26890320cb33a866c20e8da087e1a682fe8c6df
jz0001 <jinming.zhang.92@gmail.com>
2019-10-17 09:34:56 -0400

    Done.

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,50 @@
 class DogsController < ApplicationController
+
+    def index
+        @dogs = Dog.all
+    end
+
+    def new
+        @dog = Dog.new  
+        @employees = Employee.all  
+    end
+
+    def show
+        @dog = Dog.find(params[:id])
+    end
+    
+    def create
+        @dog = Dog.create(dog_params)
+
+        if @dog.save
+            redirect_to dog_path(@dog)
+        else
+            redirect_to new_dog_path
+        end
+    end
+
+
+    def edit
+        @dog = Dog.find(params[:id])
+    end
+
+
+    def update
+        @dog = Dog.find(params[:id])
+        @dog.update(dog_params)
+        redirect_to dog_path(@dog)
+    end
+
+    def destroy
+        @dog = Dog.find(params[:id])
+        @dog.destroy
+        redirect_to new_dog_path
+        
+    end
+
+    private
+
+    def dog_params
+        params.require(:dog).permit(:name, :breed, :age)
+    end
 end
